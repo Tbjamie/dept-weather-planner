39e9d4e97686db58296f863f2e5adf405137133e
Tbjamie <144114217+Tbjamie@users.noreply.github.com>
2025-05-25 21:24:47 +0200

    feat: finished homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,152 @@
-import Image from "next/image";
+import Accordeon from "../components/Accordeon";
+import ActivityCard from "../components/ActivityCard";
+import Button from "../components/Button";
+import Input from "../components/Input";
+import WeatherCard from "../components/WeatherCard";
 
-export default function Home() {
+interface WeatherCard {
+  temperature: {
+    temp: number;
+    metric: string;
+  };
+  weatherInfo: {
+    minTemp: number | null;
+    maxTemp: number | null;
+    title: string;
+    description: string;
+  }[];
+}
+
+interface ActivityCard {
+  activities: {
+    title: string;
+    imageUrl: string;
+    url: string;
+    minTemp: number | null;
+    maxTemp: number | null;
+    description: string;
+  }[];
+}
+
+export default async function Home() {
+  const weatherFetch = await fetch(
+    "https://dtnl-frontend-case.vercel.app/api/get-weather"
+  );
+
+  if (!weatherFetch.ok) {
+    throw new Error("Weather fetch failed");
+  }
+
+  const weatherData: WeatherCard = await weatherFetch.json();
+
+  const { temperature, weatherInfo } = weatherData;
+
+  const currentWeather = weatherInfo.find(
+    (weather) =>
+      (weather.minTemp === null || temperature.temp >= weather.minTemp) &&
+      (weather.maxTemp === null || temperature.temp <= weather.maxTemp)
+  );
+
+  const activityFetch = await fetch(
+    "https://dtnl-frontend-case.vercel.app/api/get-activities"
+  );
+
+  if (!activityFetch.ok) {
+    throw new Error("Activity fetch failed");
+  }
+
+  const activityData: ActivityCard = await activityFetch.json();
+
+  const { activities } = activityData;
+
+  const relevantActivities = activities.filter((activity) => {
+    return (
+      (activity.minTemp === null || temperature.temp >= activity.minTemp) &&
+      (activity.maxTemp === null || temperature.temp <= activity.maxTemp)
+    );
+  });
+
+  const irrelevantActivities = activities.filter((activity) => {
+    const { minTemp, maxTemp } = activity;
+    return (
+      (minTemp !== null && temperature.temp < minTemp) ||
+      (maxTemp !== null && temperature.temp > maxTemp)
+    );
+  });
+
+  console.log("Activities:", activities);
+  console.log("Relevant Activities:", relevantActivities);
+  console.log("Irrelevant Activities:", irrelevantActivities);
+
+  const relevantActivityCards = relevantActivities.map((activity) => {
+    return (
+      <ActivityCard
+        key={activity.title}
+        title={activity.title}
+        description={activity.description}
+        imageUrl={activity.imageUrl}
+        href={activity.url}
+      />
+    );
+  });
+
+  const irrelevantActivityCards = irrelevantActivities.map((activity) => {
+    return (
+      <ActivityCard
+        key={activity.title}
+        title={activity.title}
+        description={activity.description}
+        imageUrl={activity.imageUrl}
+        href={activity.url}
+      />
+    );
+  });
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="lg:grid lg:gap-3 max-w-[1440px] lg:grid-cols-[repeat(24, minmax(0, 1fr))] mx-auto">
+      <div className="lg:col-span-12 lg:row-start-1">
+        <section className="bg-purple text-white px-fluid-main pt-fluid-main-top flex flex-col gap-[1.875rem] pb-5 lg:pb-28 before:content-[''] before:absolute before:top-0 before:left-0 before:w-full before:h-full before:bg-purple before:-translate-x-full relative">
+          <h1>DEPT® weather planner</h1>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+          <Accordeon />
+        </section>
+
+        <section className="px-fluid-main bg-black-50 py-5 lg:py-10 before:content-[''] before:absolute before:top-0 before:left-0 before:w-full before:h-full before:bg-black-50 before:-translate-x-full relative">
+          <h2 className="!text-lg leading-7 mb-5">
+            Want to get a daily forecast?
+          </h2>
+
+          <form
+            action="/subscribe"
+            method="POST"
+            className="flex flex-col lg:flex-row gap-5 lg:gap-3.5 font-sans"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <Input placeholder="Enter your e-mailaddress" />
+            <Button buttonText="Submit" type="submit" />
+          </form>
+        </section>
+      </div>
+
+      <section className="px-fluid-main pt-5 lg:pt-[1.875rem] pb-12 lg:col-span-12 lg:row-start-1">
+        <WeatherCard
+          weather={weatherData.temperature?.temp}
+          title={currentWeather.title.replace(
+            "{{ CELCIUS }}",
+            `${temperature.temp}`
+          )}
+          description={currentWeather.description}
+        />
+
+        <h2 className="mt-[1.875rem] mb-2.5">Some things you could do:</h2>
+        <div className="flex flex-col gap-[1.875rem]">
+          {relevantActivityCards}
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+        <h2 className="mt-[1.875rem] mb-2.5">Some things you should not do:</h2>
+        <div className="flex flex-col gap-[1.875rem]">
+          {irrelevantActivityCards}
+        </div>
+      </section>
     </div>
   );
 }
