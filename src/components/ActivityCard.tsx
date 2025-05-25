b686f8a8a5e4370aac8f42cbbe4203b07f5e0c25
Tbjamie <144114217+Tbjamie@users.noreply.github.com>
2025-05-25 20:37:12 +0200

    feat: added conditional rendering to ActivityCard

--- src/components/ActivityCard.tsx
+++ src/components/ActivityCard.tsx
@@ -14,17 +14,23 @@ export default function ActivityCard({
   return (
     <a href={href} className="lg:flex lg:gap-3">
       <div className="w-full aspect-[2.6] overflow-clip mb-5 lg:mb-0 lg:aspect-square lg:w-[6.875rem] lg:h-[6.875rem]">
-        <Image
-          className="object-cover w-full h-full"
-          src={imageUrl}
-          alt={title}
-          width={335}
-          height={130}
-        />
+        {imageUrl ? (
+          <Image
+            className="object-cover w-full h-full"
+            src={imageUrl}
+            alt={title}
+            width={335}
+            height={130}
+          />
+        ) : null}
       </div>
+
       <div className="pb-5 lg:pb-6 border-b border-solid border-black-100">
-        <h3 className="!text-fluid-activity mb-1 lg:mb-0">{title}</h3>
-        <p className="font-light">{description}</p>
+        {title ? (
+          <h3 className="!text-fluid-activity mb-1 lg:mb-0">{title}</h3>
+        ) : null}
+
+        {description ? <p className="font-light">{description}</p> : null}
       </div>
     </a>
   );
