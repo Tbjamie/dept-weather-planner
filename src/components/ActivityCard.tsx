4a74468680440f749098f7b3f1fd579d448777a4
Tbjamie <144114217+Tbjamie@users.noreply.github.com>
2025-05-25 20:18:48 +0200

    feat: finished ActivityCard component

---
+++ src/components/ActivityCard.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+
+export default function ActivityCard({
+  title = "",
+  description = "",
+  imageUrl = "",
+  href = "",
+}: {
+  title?: string;
+  description?: string;
+  imageUrl?: string;
+  href?: string;
+}) {
+  return (
+    <a href={href} className="lg:flex lg:gap-3">
+      <div className="w-full aspect-[2.6] overflow-clip mb-5 lg:mb-0 lg:aspect-square lg:w-[6.875rem] lg:h-[6.875rem]">
+        <Image
+          className="object-cover w-full h-full"
+          src={imageUrl}
+          alt={title}
+          width={335}
+          height={130}
+        />
+      </div>
+      <div className="pb-5 lg:pb-6 border-b border-solid border-black-100">
+        <h3 className="!text-fluid-activity mb-1 lg:mb-0">{title}</h3>
+        <p className="font-light">{description}</p>
+      </div>
+    </a>
+  );
+}
